64e1e3c9d646c5bad06eb641e4215fa6d89b6b72
nakajima97 <kituneudon97@gmail.com>
2023-08-14 10:45:56 +0900

    ユニーク制約が漏れていたので追加

--- database/migrations/2023_08_14_103619_create_genres_table.php
+++ database/migrations/2023_08_14_103619_create_genres_table.php
@@ -17,6 +17,8 @@ class CreateGenresTable extends Migration
             $table->id();
             $table->string('name');
             $table->timestamps();
+
+            $table->unique('name');
         });
     }
 
