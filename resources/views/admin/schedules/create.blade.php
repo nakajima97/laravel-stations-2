3c9f3dcb3446b97824679182aa4feda7ac75216d
nakajima97 <kituneudon97@gmail.com>
2023-08-17 08:50:22 +0900

    スケジュール作成画面でバリデーションエラー時に前の値を保持するようにした

--- resources/views/admin/schedules/create.blade.php
+++ resources/views/admin/schedules/create.blade.php
@@ -17,19 +17,19 @@
         <input type="hidden" name="movie_id" value="{{ $id }}">
         <div>
             <label for="start_time_date">開始日付</label>
-            <input type="date" name="start_time_date" id="start_time_date">
+            <input type="date" name="start_time_date" id="start_time_date" value="{{ old('start_time_date') }}">
         </div>
         <div>
             <label for="start_time_time">開始時間</label>
-            <input type="time" name="start_time_time" id="start_time_time">
+            <input type="time" name="start_time_time" id="start_time_time" value="{{ old('start_time_time') }}">
         </div>
         <div>
             <label for="end_time_date">終了日付</label>
-            <input type="date" name="end_time_date" id="end_time_date">
+            <input type="date" name="end_time_date" id="end_time_date" value="{{ old('end_time_date') }}">
         </div>
         <div>
             <label for="end_time_time">終了時間</label>
-            <input type="time" name="end_time_time" id="end_time_time">
+            <input type="time" name="end_time_time" id="end_time_time" value="{{ old('end_time_time') }}">
         </div>
         <input type="submit" value="作成">
     </form>
